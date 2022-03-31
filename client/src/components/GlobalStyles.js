129bcd086e72b6fc0cf2ba88324b51e481273cf4
Luke Hayes <lukehayesa@gmail.com>
2022-03-31 16:07:39 -0400

    <test>

---
+++ client/src/components/GlobalStyles.js
@@ -0,0 +1,83 @@
+import { createGlobalStyle } from "styled-components";
+
+export const theme = {
+  accentColor: "#0010f2",
+  hoverAccentColor: "#C64500",
+  fontColor: "#323232",
+  headingFont: "Dela Gothic One, sans-serif",
+  contentFont: "Lato, sans-serif",
+};
+
+export default createGlobalStyle`
+  *,
+  *:before,
+  *:after {
+      box-sizing: border-box;
+      -webkit-font-smoothing: antialiased;
+      scroll-behavior: smooth;
+  }
+  body {
+  margin: 0 auto;
+    /* background-image: url('/images/backgroundImg.jpg'); */
+  background-size: cover;
+  background-position: center;
+  box-shadow:inset 0 0 0 2000px rgb(48,48,48, 0.4);
+  overflow-x: hidden;
+  background-repeat: no-repeat;
+  }
+  
+  
+html, body, div,
+input, button, select, option, p,
+text, a {
+    font-family: ${theme.contentFont};
+    line-height: 2;
+    font-weight: 500;
+}
+h1, h2, h3, h4, h5, h6 {
+  font-family: ${theme.headingFont};
+  text-transform: uppercase;
+}
+ul {
+  list-style: none;
+  padding: 0;
+}
+h2, h3, h4 {
+  font-weight: 700;
+}
+a {
+  text-decoration: none;
+  &:visited {
+    text-decoration: none;
+  }
+}
+.accentBtn {
+  border-radius: 5px;
+  background-color: ${theme.accentColor};
+  color: white;
+  border: none;
+  padding: 8px 15px;
+  font-family: ${theme.contentFont};
+  text-transform: uppercase;
+  font-size: 16px;
+  font-weight: 600;
+  cursor: pointer;
+  
+  &:hover {
+    background-color: ${theme.hoverAccentColor};
+  }
+  @media screen and (min-width: 900px) {
+    &:active {
+    animation: scaleIn 0.3s ease-in-out forwards;
+  }
+  }
+}
+@keyframes scaleIn {
+  from {
+    transform: scale(0.8)
+  }
+  to {
+    transform: scale(1);
+  }
+}
+  `;
