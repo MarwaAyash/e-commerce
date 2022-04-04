a00f7f56c1c23d4568a8bc905cf336c01e059cc5
“Marwa <marwa_ayash@hotmail.com>
2022-04-04 10:12:00 -0400

    update

--- client/src/components/Confirmation.js
+++ client/src/components/Confirmation.js
@@ -1,99 +1,179 @@
-import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
-import styled from "styled-components";
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+import { AppContext } from './AppContext';
+import { useHistory } from 'react-router-dom';
+import { FaCheck } from "react-icons/fa";
 
 const Confirmation = () => {
-        const [order, setorder] = useState({});
-        useEffect(() => {
-        const id = localStorage.getItem("orderId");
-        fetch(`/api/get-order/${id}`)
-            .then((res) => {
-            return res.json();
-            })
-            .then((json) => {
-            setorder(json.data);
-            });
-        }, []);
-    
+
+        const { purchased, formValue, setFormValue } = useContext(AppContext)
+
+        const history = useHistory();
+
+        let count = 0;
+        purchased.map((item) => {
+            let price = item.product.price;
+            let removeDollarSign = price.substr(1)
+            return (count =
+                count + Number(item.quantityOfProduct) * Number(removeDollarSign)).toFixed(2)
+        });
+
         return (
-        <>
-            {order !== {} && (
-            <MainContainer>
-                <MessageConfirmed>Your order has been confirmed!</MessageConfirmed>
-                <OrderConfirmed>
-                <InfoContainer>
-                    <Label>Order #: </Label>
-                    <Info>{order._id}</Info>
-                </InfoContainer>
-                <InfoContainer>
-                    <Label>Customer Name: </Label>
-                    <Info>{order.name}</Info>
-                </InfoContainer>
-                <InfoContainer>
-                    <Label>Shipping Address: </Label>
-                    <Info>{order.address}</Info>
-                </InfoContainer>
-                {/* <InfoContainer>
-                    <Label>Number of Items:</Label>
-                    <Info>{order.total_quantity}</Info>
-                </InfoContainer>
-                <Line />
-                <InfoContainer>
-                    <Label>Total Price:</Label>
-                    <Info>${order.total_price}</Info>
-                </InfoContainer> */}
-                </OrderConfirmed>
-            </MainContainer>
-            )}
-        </>
-        );
-};
+            <Wrapper>
+                {" "}
+                {formValue.firstName === "" && history.push("/confirmation")}
+                <BodyBody>
 
-const MainContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    width: 100vw;
-    background-color: grey;
-`;
+                    <OrderHeader>Order Confirmed</OrderHeader>
+                    <SexyIcon><FaCheck/></SexyIcon>
+                    <FurtherDetails>
+                        {" "}
+                        <p>Thank you for your purchase!</p>
+                        <p>We will be sending a confirmation email to {" "}
+                        <ItemsInfo>{formValue.email}</ItemsInfo>verrrry soon.</p>
+                        </FurtherDetails>
+                        <Divider/>
+                        <p>
+                            <MoarDeets>Order Number: </MoarDeets>
+                            <span>{formValue.orderNum}</span></p>
+                            <p>
+                                {" "}
+                                <MoarDeets>Date: </MoarDeets> <span>{formValue.date}</span></p>
+                                <p>
+                                    <strong>Name</strong>: {formValue.firstName} {formValue.lastName}
+                                </p>
+                                <MoarDeets>Shipping Address: </MoarDeets>
+                                <ShippingAddress>
+                                    {formValue.address}, {formValue.city}, {formValue.province}, {" "}
+                                    {formValue.country}
+                                </ShippingAddress>
+                                <p>
+                                    <MoarDeets>
+                                        Credit Card Ending With {" "}
+                                        <span>
+                                            {formValue.creditCardNum.slice(
+                                                formValue.creditCardNum.length - 4
+                                            )}
+                                        </span>
+                                    </MoarDeets>
+                                </p>
 
-const MessageConfirmed = styled.h2``;
+                                <Products>
+                                    <ul>
+                                        {purchased.map((item) => {
+                                            <Item>
+                                                <p>
+                                                    <ItemsInfo>{item.quantityOfProduct}</ItemsInfo> {item.product.name}
+                                                </p>
+                                                <p>{item.product.price}</p>
+                                            </Item>
+                                        })}
+                                    </ul>
+                                </Products>
+                                <Divider />
+                                <SubTotal>
+                                    <p>Subtotal</p>
+                                    <p>CAD ${Number(count).toFixed(2)}</p>
+                                </SubTotal>
+                                <SubTotal>
+                                    <p>Shipping</p>
+                                    <p>Expensive</p>
+                                </SubTotal>
+                                <SubTotal>
+                                    <p>Tax</p>
+                                    <p>CAD ${Number(count * 0.13).toFixed(2)}</p>
+                                </SubTotal>
+                                <Divider/>
+                                <Total>
+                            <p>Total</p>
+                        <p>CAD ${Number(Number(count) + Number(count) * 0.13).toFixed(2)}</p>
+                    </Total>
+                </BodyBody>
+            </Wrapper>
+        )
+}
 
-const OrderConfirmed = styled.div`
-    display: flex;
-    flex-direction: column;
+
+const Wrapper = styled.div`
     background-color: white;
-    height: auto;
-    width: 500px;
-    border-radius: 7px;
-    padding: 20px 50px 20px;
-    
+    margin: -20px 0px 0px 0px 0px;
+    display: flex;
+    flex-direction: row;
+    min-height: 300px;
+    font-size: 14px;
 `;
 
-const InfoContainer = styled.div`
+const SexyIcon = styled.div`
+    display: flex;
+    justify-content: center;
+    font-size: 50px;
+`
+const OrderHeader = styled.h2`
+    font-size: 34px;
+    text-align: center;
+    margin-top: -10px;
+`;
+
+const FurtherDetails = styled.div`
+    text-align: center;
+    margin-bottom: 20px;
+    font-size: 26px;
+`;
+
+const Products = styled.div`
+    font-size: 14px;
+`;
+
+const ShippingAddress = styled.p`
+    font-weight: 700;
+`;
+
+const ItemsInfo = styled.span`
+    font-weight: 700;
+    color: black;
+    font-size: 14px;
+`;
+
+const MoarDeets = styled.span`
+    font-weight: 700;
+`;
+
+const BodyBody = styled.div`
+    flex: 1;
+    margin-left: 10px;
+    padding: 80px;
+`;
+
+const Divider = styled.hr`
+    border: 0.5px solid black;
+    width: 100%;
+    margin-top: 20px;
+    margin-bottom: -12px;
+`;
+
+const Item = styled.li`
+    width: 100%;
     display: flex;
     justify-content: space-between;
 `;
 
-const Label = styled.h2`
-    font-weight: 800;
+const Total = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
 `;
 
-const Info = styled.h2`
-    font-weight: 200;
-    color: grey;
-    text-align: right;
-`;
-
-const Line = styled.hr`
-    border: 0;
-    height: 0;
-    border-top: 1px solid rgba(0, 0, 0, 0.1);
-    border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+const SubTotal = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: -30px;
+    padding: 0;
+    font-size: 15px;
 `;
 
 
-export default Confirmation;
+
+
+
+ export default Confirmation; 
